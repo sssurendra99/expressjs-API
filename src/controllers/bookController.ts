2fb815634b7d822a3397250f97f12a6e9cecd571
sumalsurendra1999@gmail.com <sumalsurendra1999@gmail.com>
2025-01-11 14:42:39 +0530

    Added the prisma and cached it

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,1 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 
+export interface Context {
+    prisma: PrismaClient
+}
+
+const globalForPrisma = globalThis as unknown as {prisma: PrismaClient}
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+globalForPrisma.prisma = prisma
+
+export const context: Context = {
+    prisma: prisma,
+}
